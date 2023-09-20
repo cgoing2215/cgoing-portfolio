e93ac6ccd7a3809d18f4962eefdf8d6ea262b533
Caroline Going <carolinegoing2@gmail.com>
2023-09-20 10:36:54 -0400

    Updated language for about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,20 +12,21 @@ function About() {
         </p>
         <p>
           In Spring 2023, I made a career pivot from tech recruiting to web development & UI design, with 
-          the intent of building websites and applications that exude sophistication, intuitive design, and
-          user-friendliness.
+          the intent of building websites and applications that exude sophistication, user-friendliness, and
+          modern design.
         </p>
         <p>
           Within two months, I completed the <a href="https://skillcrush.com/">SkillCrush</a> Break-Into-Tech bootcamp, 
-          learning FE languages alongside fundamental web design principles, all while building out numerous <a href="#portfolio-section-container">projects</a>, sites and 
-          applications that allow for varied user capabilities. 
+          learning FE languages alongside fundamental web design principles. I built out numerous <a href="#portfolio-section-container">projects</a>, sites and 
+          applications that allow for varied user experiences, and crafted designs that could appeal to broad audiences. 
         </p>
         <p>
           My tech recruiting background ingrained a deep-rooted commitment to empathic user-focused design, helping me to
-          think more strategically about what motivates a candidate and how to inspire them to action. 
+          think more strategically about what motivates a candidate and how to inspire them to action. My hope is to take
+          this approach with me into my web development career, and bring a new perspective to a technical team.
         </p>
         <p>
-          Outside of coding, you can find me listening to podcasts in Prospect Park, wandering new neighborhoods in the city,
+          Outside of coding, you can find me listening to podcasts in Prospect Park, wandering new neighborhoods throughout NYC,
           or sitting in a favorite coffee shop, finding for the latest trends in web dev and UI design. 
         </p>
         <div id="lang-proficiencies">

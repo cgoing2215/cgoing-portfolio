afb8d00809a2c0ceecc19d8dd2581c545757836f
Caroline Going <carolinegoing2@gmail.com>
2023-10-02 16:07:32 -0400

    Changed color of span within get-in-touch line in contact section to black for visibility

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,7 +7,7 @@ function Contact() {
       <h3>CONTACT</h3>
       <div id="contact-call-to-action">
         <div id="get-in-touch-line">
-          <h5>Let's <span id="turquoise">connect</span>.</h5>
+          <h5>Let's connect.</h5>
         </div>
         <a href="mailto:hi@carolinegoing.com"><button>email</button></a>
       </div>
